f1e834a9ca4a4104b6a841327e43bc894153f725
gu-xiaoqing-hyron <gu.xiaoqing@mcc-system.jp>
2017-08-03 11:25:18 +0900

    TEST

--- sample.c
+++ sample.c
@@ -7,6 +7,12 @@ void l_func(int a)
 	return;
 }
 
+void l_func_new(int c)
+{
+	printf("func_new test\n");
+	return;
+}
+
 int main(int argc, char *argv[])
 {
 	int b = 0;
@@ -14,6 +20,8 @@ int main(int argc, char *argv[])
 	printf("main test b=%d\n", b);
 	
 	l_func(b);
+	
+	l_func_new(b);
 	
 	return 0;
 }
